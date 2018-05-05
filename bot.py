3f7c25150b42bee4e8dbaf75e46b9cdeee022b86
evelister <33561840+evelister@users.noreply.github.com>
2018-05-05 17:05:13 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,22 @@
+import time
+from wxpy import *
+
+bot = Bot(cache_path='wxpy.pkl')
+
+def get(i):
+    with open('晚安.txt', 'r', encoding='utf-8') as f:
+        line = f.readlines()[i]
+    return line
+
+def send(i):
+    myfriend = bot.friends().search('ANAN')[0]
+    myfriend.send(get(i))
+    i += 1
+
+def main():
+    for i in range(3650):
+        send(i)
+        time.sleep(5)
+
+if __name__ == '__main__':
+    main()
